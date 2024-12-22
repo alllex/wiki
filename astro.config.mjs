3912c25f6680a2a944c14c82b939d873158d7b3a
Alex Semin <software@alllex.me>
2024-12-22 17:58:22 +0100

    Add GitHub pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://alllex.github.io',
     integrations: [
         starlight({
             title: 'Wiki by Alex',
