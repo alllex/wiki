15c1f9e679a361f850db5f6492fd3423037d717c
Alex Semin <software@alllex.me>
2024-12-22 18:05:47 +0100

    Add repo suffix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://alllex.github.io',
+    base: 'wiki',
     integrations: [
         starlight({
             title: 'Wiki by Alex',
